fe3188323d24b64e8826398bd5cc9c9969d07cb2
INGMMV01 <kitos78@gmail.com>
2018-02-08 22:03:50 +0100

    video 54, 55

--- src/app/components/heroe/heroe.component.ts
+++ src/app/components/heroe/heroe.component.ts
@@ -1,14 +1,29 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { HeroesService, Heroe } from '../../servicios/heroes.service';
+
+
 
 @Component({
   selector: 'app-heroe',
-  templateUrl: './heroe.component.html'  
+  templateUrl: './heroe.component.html'
 })
-export class HeroeComponent implements OnInit {
+export class HeroeComponent {
 
-  constructor() { }
+  heroe: any = {};
+  mivar: string = 'kitos';
 
-  ngOnInit() {
+
+  constructor(private activatedRoute: ActivatedRoute,
+    private _heroeService: HeroesService) {
+    this.activatedRoute.params.subscribe(params => {
+      this.heroe = _heroeService.getHeroe(params['id']);
+      console.log(params['id']);
+      console.log(this.heroe);
+      console.log(_heroeService.getHeroes());
+    });
   }
 
+
 }
+
