e4128d858ec5f91116e73bc58f85fccf7cd502cb
INGMMV01 <kitos78@gmail.com>
2018-02-26 19:16:07 +0100

    video 55

--- src/app/components/shared/navbar/navbar.component.ts
+++ src/app/components/shared/navbar/navbar.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { HeroesService } from '../../../servicios/heroes.service';
+
 
 @Component({
   selector: 'app-navbar',
@@ -6,14 +8,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavbarComponent implements OnInit {
 
+  heroesService: HeroesService;
 
-  constructor() { }
+  constructor() {
+    this.heroesService = new HeroesService();
+   }
 
   ngOnInit() {
   }
 
+  /*
   buscarHeroe = function(termino: string) {
     console.log(termino);
     console.log('kitos');
+    alert('kitos');
+    let i:number = 1/0;
+  }
+  */
+
+  buscarHeroe(termino:string){
+    this.heroesService.buscarHeroes(termino);
+
   }
 }
